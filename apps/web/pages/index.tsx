e1b363f03bc9bde202062f662487d56f3156642d
anilkumarsheoran <anilkumar.sheoran@gmail.com>
2022-09-13 11:56:18 +1000

    uat 1

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web will merge to uat</h1>
       <h2>This is {process.env.NEXT_PUBLIC_ENV_NAME} env</h2>
       <Button />
     </div>
