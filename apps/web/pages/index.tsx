f0ed71c12fe1312ea4c4c29d2d0fd090a60a3e87
anilkumarsheoran <anilkumar.sheoran@gmail.com>
2022-09-13 11:05:35 +1000

    adding new branch

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Staging Web</h1>
+      <h1>Testing 2 Web</h1>
       <h2>This is {process.env.NEXT_PUBLIC_ENV_NAME} env</h2>
       <Button />
     </div>
